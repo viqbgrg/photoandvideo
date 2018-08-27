6d4be57830c2179aca24b6a5a920d50ccbe05ad9
viqbgrg <viqbgrg@gmail.com>
2018-08-27 19:47:04 +0800

    Update PhotoAndVideo.m

--- src/ios/PhotoAndVideo.m
+++ src/ios/PhotoAndVideo.m
@@ -1,37 +1,37 @@
 //
-//  photoandvideo.m
+//  PhotoAndVideo.m
 //  test5
 //
 //  Created by Embrace on 2017/6/1.
 //
 //
 
-#import "photoandvideo.h"
+#import "PhotoAndVideo.h"
 
-@implementation photoandvideo
+@implementation PhotoAndVideo
 - (void)takePhotoOrVideoMethod:(CDVInvokedUrlCommand *)command {
     self.hasPendingOperation = YES;
-    __weak photoandvideo* weakSelf = self;
+    __weak PhotoAndVideo* weakSelf = self;
     weakSelf.latestCommand = command;
     weakSelf.mediaVC = [[RCMediaViewController alloc] init];
     weakSelf.mediaVC.plugin = self;
-
+    
     [self.viewController presentViewController:weakSelf.mediaVC animated:YES completion:nil];
 }
 
 
 
 -(void)dismissCamera {
-    __weak photoandvideo* weakSelf = self;
+    __weak PhotoAndVideo* weakSelf = self;
     [weakSelf.mediaVC dismissViewControllerAnimated:YES completion:nil];
 }
 
 -(void) capturedImageOrVideoWithPath:(NSString*)GetPath {
     [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:GetPath] callbackId:self.latestCommand.callbackId];
-
+    
     // Unset the self.hasPendingOperation property
     self.hasPendingOperation = NO;
-
+    
     // Hide the picker view
     [self.viewController dismissViewControllerAnimated:YES completion:nil];
 }
